fc1bba6820d17d46fa5b09f99aa8fa01535a69ea
Coreydonohue <cqdonohue@gmail.com>
2023-10-16 16:33:52 -0400

    randomized group code added

--- src/server/api/role.js
+++ src/server/api/role.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-const protection = require("../middleware") ;
+const protection = require("../middleware");
 
 router.get("/", async (req, res, next) => {
   try {
