9520439ec180e5e5e87a7b0ae062ace24ebe5865
SerenaTPhan <pttnhann@gmail.com>
2023-10-17 13:15:10 -0500

    questionRoutes

--- src/server/api/questions.js
+++ src/server/api/questions.js
@@ -2,25 +2,24 @@ const express = require("express");
 const router = express.Router();
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+const protection = require("../middleware");
 
 
-
-router.post("/", async (req, res, next) => {
-  const { title } = req.body;
-  const user = 1 //update based on your current postico dummy data
+router.post("/", protection, async (req, res, next) => {
+  const { title, group_id } = req.body;
 
   try {
     const groupCreator = await prisma.role.findFirst({
       where: {
-        user_id: user,
+        user_id: req.user.id,
         is_creator: true,
       },
     });
     const createdQuestion = await prisma.question.create({
       data: {
         title,
-        group_id: groupCreator.group_id,
-        user_id: groupCreator.id,
+        group_id,
+        user_id: groupCreator.user_id,
         is_active: true,
   
       },
@@ -32,83 +31,5 @@ router.post("/", async (req, res, next) => {
   }
 });
 
-
-// router.get("/", async (req, res, next) => {
-//   try {
-//     const allQuestions = await prisma.Question.findMany();
-//     res.send(allQuestions)
-//   } catch (err) {
-//     next(err);
-//   }
-// });
-
-// router.get("/:id", async (req, res, next) => {
-//     try {
-//       const question = await prisma.Question.findUnique({
-//         where: {
-//           id: Number(req.params.id),
-//         },
-//       });
-//       res.send(question);
-//     } catch (err) {
-//       next(err);
-//     }
-//   });
-// router.get("/group/:id", async (req, res, next) => {
-//     try {
-//       const activeQuestion = await prisma.Question.findMany({
-//         where: {
-//             group_id: Number(req.params.id),
-//             is_active: true,
-//         },
-//       });
-//       res.send(activeQuestion);
-//     } catch (err) {
-//       next(err);
-//     }
-//   });
-
-//   router.delete("/:id",  async (req, res, next) => {
-
-//     try {
-//       const question = await prisma.Question.delete({
-//         where: {
-//           id: Number(req.params.id),
-//         },
-//       });
-//       res.send(question);
-//     } catch (err) {
-//       next(err);
-//     }
-//   });
-
-//   router.post("/", async (req, res, next) => {
-//     try {
-//       const question = await prisma.Question.create({
-//         data: req.body,
-//       });
-//       console.log('req body from post request', req.body);
-//       res.send(question);
-//     } catch (err) {
-//       console.error("Error adding group", err)
-//       res.status(500).send(err.message);
-//       next(err);
-//     }
-//   });
-
-//   router.put("/:id",  async (req, res, next) => {
-
-//     try {
-//       const question = await prisma.Question.update({
-//         where: {
-//           id: Number(req.params.id),
-//         },
-//         data: req.body,
-//       });
-//       res.send(question);
-//     } catch (err) {
-//       next(err);
-//     }
-//   });
 
 module.exports = router
--- src/server/middleware.js
+++ src/server/middleware.js
@@ -1,17 +1,19 @@
 const jwt = require("jsonwebtoken");
 const process = require("process");
+
+
 const protection = async (req, res, next) => {
-    const token = req.headers.authorization
 
+    const bearerToken = req.headers.authorization;
 
-    if (!token) {
+    if (!bearerToken) {
         return res.status(401).send("No token provided.");
     }
 
+    const token = bearerToken.split(' ')[1];
 
     try {
         req.user = jwt.verify(token, process.env.JWT);
-        console.log("middleware req.user", req.user)
         next();
     } catch (error) {
         return res.status(403).send("Failed to authenticate token.")
