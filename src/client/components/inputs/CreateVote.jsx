c71045417a2ac33d2a69add2d62e9a943a1b9ed5
SerenaTPhan <pttnhann@gmail.com>
2023-10-20 11:06:40 -0500

    voteLogicDone

--- src/client/components/inputs/CreateVote.jsx
+++ src/client/components/inputs/CreateVote.jsx
@@ -1,7 +1,14 @@
+import React, { useState, Fragment } from "react";
 import { useCreateVoteMutation } from "../../reducers/api";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 const CreateVote = ({ questionId, submissionId }) => {
   const [createVote] = useCreateVoteMutation();
+
+  const [active, setActive] = useState(false);
+  const handleClick = () => {
+    setActive(!active);
+  };
 
   const onCreateVote = async () => {
     await createVote({ questionId, submissionId })
@@ -14,9 +21,17 @@ const CreateVote = ({ questionId, submissionId }) => {
   };
 
   return (
-    <section>
-      <button onClick={onCreateVote}>Vote For This Link</button>
-    </section>
+    <Fragment>
+      <button
+        onClick={function (event) {
+          onCreateVote();
+          handleClick();
+        }}
+        style={{ backgroundColor: active ? "green" : "white" }}
+      >
+        <FontAwesomeIcon icon={faHeart} />
+      </button>
+    </Fragment>
   );
 };
 
