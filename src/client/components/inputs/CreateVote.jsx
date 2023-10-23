537e49066d7fb23895fee7baf2f01755f4614b23
Coreydonohue <cqdonohue@gmail.com>
2023-10-23 16:18:44 -0400

    added votes to web socket, embeded youtube videos with updated logic for add submission

--- src/client/components/inputs/CreateVote.jsx
+++ src/client/components/inputs/CreateVote.jsx
@@ -2,7 +2,14 @@ import React, { useState, Fragment } from "react";
 import { useCreateVoteMutation } from "../../reducers/api";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { io } from "socket.io-client";
+
+
 const CreateVote = ({ questionId, submissionId }) => {
+
+  const socket = io.connect("http://localhost:3000");
+  socket.on("connect", () => {});
+
   const [createVote] = useCreateVoteMutation();
 
   const [active, setActive] = useState(false);
@@ -14,6 +21,7 @@ const CreateVote = ({ questionId, submissionId }) => {
     await createVote({ questionId, submissionId })
       .then(() => {
         console.log("voted");
+        socket.emit("new_vote", submissionId);
       })
       .catch(() => {
         console.log("error");
