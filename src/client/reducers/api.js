af5a6fc0baf81b2577c526347088ab26d29489c2
SerenaTPhan <pttnhann@gmail.com>
2023-10-26 10:00:12 -0500

    create role for non-creator update

--- src/client/reducers/api.js
+++ src/client/reducers/api.js
@@ -72,9 +72,6 @@ export const votingApi = createApi({
       query: (body) => ({
         url: "api/role",
         method: "POST",
-        where: {
-          question_id: Number(req.body.questionId),
-        },
         body: body,
       }),
     }),
