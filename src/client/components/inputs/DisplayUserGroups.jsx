9abddef017bf28a90dffee3eaa3034d74ce5e3ab
SerenaTPhan <pttnhann@gmail.com>
2023-11-02 10:04:35 -0500

    dropDowncustomization

--- src/client/components/inputs/DisplayUserGroups.jsx
+++ src/client/components/inputs/DisplayUserGroups.jsx
@@ -8,6 +8,7 @@ function DisplayUserGroups() {
   const userId = useSelector((state) => state.auth.credentials.user.userId);
   const { data: userGroups, error, isLoading } = useGetUserGroupsByRolesQuery();
   const [isOpen, setIsOpen] = useState(false);
+  const [isOpen2, setIsOpen2] = useState(false);
 
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>Error fetching users groups: {error.message}</p>;
@@ -47,14 +48,14 @@ function DisplayUserGroups() {
           {" "}
           Joined Group{" "}
           <span
-            onClick={() => setIsOpen(!isOpen)}
-            onBlur={() => setIsOpen(false)}
+            onClick={() => setIsOpen2(!isOpen2)}
+            onBlur={() => setIsOpen2(false)}
           >
             {" "}
             <FontAwesomeIcon icon={faCaretDown} />
           </span>
         </h2>
-        {isOpen && (
+        {isOpen2 && (
           <div className="dropdown-content">
             <ul>
               {sheep.map((group) => (
