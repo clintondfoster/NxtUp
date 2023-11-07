39d28db011d7fbb49cc414e7ffe026a4b7cebfa3
SerenaTPhan <pttnhann@gmail.com>
2023-11-07 16:39:55 -0600

    checking for valid youtube link

--- src/client/components/GroupPage/DeleteGroup.jsx
+++ src/client/components/GroupPage/DeleteGroup.jsx
@@ -5,41 +5,43 @@ import { useGetCurrentUserQuery } from "../../reducers/auth";
 import "./DeleteGroup.scss";
 
 const DeleteGroup = ({ groupId }) => {
-   const { data: currentUser } = useGetCurrentUserQuery();
-   const [deleteGroup] = useDeleteGroupByCodeMutation();
-   const navigate = useNavigate();
+  const { data: currentUser } = useGetCurrentUserQuery();
+  const [deleteGroup] = useDeleteGroupByCodeMutation();
+  const navigate = useNavigate();
 
-   const { refetch } = useDeleteGroupByCodeMutation(groupId);
+  const { refetch } = useDeleteGroupByCodeMutation(groupId);
 
-   const handleDeleteGroup = async () => {
-      try {
-         const response = await deleteGroup(groupId);
-         navigate(`/home`);
-      } catch (err) {
-         console.log("Error deleting group:", err);
-      }
-   };
+  const handleDeleteGroup = async () => {
+    try {
+      const response = await deleteGroup(groupId);
+      setTimeout(() => {
+        navigate("/home");
+      }, 2000);
+    } catch (err) {
+      console.log("Error deleting group:", err);
+    }
+  };
 
-   const isAdmin = currentUser?.user?.roles?.some(
-      (role) => role.group_id === groupId && role.is_admin
-   );
+  const isAdmin = currentUser?.user?.roles?.some(
+    (role) => role.group_id === groupId && role.is_admin
+  );
 
-   if (isAdmin) {
-      return (
-         <div
-            className="delete-btn"
-            onClick={() => {
-               const confirm = window.confirm(
-                  "Are you sure you want to delete this group and all information associated with it?"
-               );
-               if (confirm === true) {
-                  handleDeleteGroup(groupId);
-               }
-            }}
-         >
-            Delete Group
-         </div>
-      );
-   }
+  if (isAdmin) {
+    return (
+      <div
+        className="delete-btn"
+        onClick={() => {
+          const confirm = window.confirm(
+            "Are you sure you want to delete this group and all information associated with it?"
+          );
+          if (confirm === true) {
+            handleDeleteGroup(groupId);
+          }
+        }}
+      >
+        Delete Group
+      </div>
+    );
+  }
 };
 export default DeleteGroup;
